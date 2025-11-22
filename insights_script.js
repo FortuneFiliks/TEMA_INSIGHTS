2aaa6332c69c3c2c2627ea7f39394bc9866969db
fortunefelix <felixfortune82@gmail.com>
2025-11-22 12:35:52 +0100

    first commit

---
+++ insights_script.js
@@ -0,0 +1,54 @@
+const get_Strategize_Word = document.querySelector(".left_word");
+
+function animate_Strategize_Word(entries){
+	entries.forEach( (entry) => {
+		if(entry.isIntersecting){
+			entry.target.classList.add("animate_strategize_class");
+		}else{
+			
+			entry.target.classList.remove("animate_strategize_class");
+		}
+		
+	});
+	
+	
+	
+}
+
+strategize_Word_Root = 
+{
+	root : null,
+	rootMargin : "0px",
+	threshold : "0.6",
+}
+
+strategize_Word_Intersection_Api = new IntersectionObserver(animate_Strategize_Word, strategize_Word_Root);
+strategize_Word_Intersection_Api.observe(get_Strategize_Word);
+
+
+const get_Convert_Word = document.querySelector(".right_word");
+
+function animate_Convert_Word(entries){
+	entries.forEach( (entry) => {
+		if(entry.isIntersecting){
+			entry.target.classList.add("animate_convert_class");
+		}else{
+			
+			entry.target.classList.remove("animate_convert_class");
+		}
+		
+	});
+	
+	
+	
+}
+
+convert_Word_Root = 
+{
+	root : null,
+	rootMargin : "0px",
+	threshold : "0.6",
+}
+
+convert_Word_Intersection_Api = new IntersectionObserver(animate_Convert_Word, convert_Word_Root);
+convert_Word_Intersection_Api.observe(get_Convert_Word);
